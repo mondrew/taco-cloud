5efd942bab351afc243ca662309f7675400ce732
mondrew <andreib2510@yandex.ru>
2022-05-18 00:04:24 +0300

    Done order, form validation and css

--- src/main/java/com/mondrew/tacocloud/app/TacoCloudApplication.java
+++ src/main/java/com/mondrew/tacocloud/app/TacoCloudApplication.java
@@ -1,9 +1,11 @@
-package com.mondrew.tacocloud;
+package com.mondrew.tacocloud.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.mondrew.tacocloud")
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
